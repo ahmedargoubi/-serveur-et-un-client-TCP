082ea03f67362ebdf91e51a1b184932b9c79afe2
Ahmed <ahmedargoubi28@gmail.com>
2024-06-22 20:24:53 +0300

    Update client.py

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connect the client
 client.connect((target_host,target_port))
 # send some data
-client.send(b"HELLO FRIEND / HTTP/1.1\r\nHost: 0.0.0.0\r\n\r\n")
+client.send(b"GET / HTTP/1.1\r\nHost: 0.0.0.0\r\n\r\n")
 # receive some data
 response = client.recv(4096)
 print(response.decode())
